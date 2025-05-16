1cdbcbe1e11ed96a68a7a1c03e56b46d6162c622
HienHg <hienhg2k3@gmail.com>
2025-05-16 11:44:49 +0700

    Write the login fixture

---
+++ src/api/login-logout/login-logout.api.ts
@@ -0,0 +1,17 @@
+import { ENDPOINTS } from "@core/const/endpoint";
+import { APIRequestContext } from "@playwright/test"
+
+export default class LoginLogoutAPI {
+    private request: APIRequestContext;
+
+    constructor(request: APIRequestContext) {
+        this.request = request
+    }
+
+    async login(email: string, password: string) {
+        const res = await this.request.post(ENDPOINTS.USER_LOGIN, { 
+            data: { email, password}
+         })
+        return res
+    }
+}
---
+++ src/pages/base-page.ts
@@ -0,0 +1,8 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class BasePage {
+    page : Page
+    constructor(page: Page) {
+        this.page = page
+    }
+}
